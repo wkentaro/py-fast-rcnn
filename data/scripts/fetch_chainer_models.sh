9d67f21ea0c1b136035770b93854c8d896ed14d6
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-02-14 12:44:54 +0900

    Fix url for fetching chainer models

--- data/scripts/fetch_chainer_models.sh
+++ data/scripts/fetch_chainer_models.sh
@@ -4,9 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 cd $DIR
 
 FILE=chainer_models.tgz
-URL="https://drive.google.com/uc?id=0B9P1L--7Wd2vVnpjcEVONGRiMUE&export=download"
+URL="https://drive.google.com/uc?id=0B9P1L--7Wd2vWjM1T28tMC1OSTQ&export=download"
 
-wget $URL -O $FILE
+gdown $URL -O $FILE
 
 echo "Unzipping file..."
 
